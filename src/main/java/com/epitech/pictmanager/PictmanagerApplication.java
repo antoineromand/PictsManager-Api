d9c1a36f10abdb465d8cd0b34b486eb48e6cf0cd
Antoine Romand <antoine.romand@epitech.eu>
2023-02-27 14:13:45 +0100

    Init api

---
+++ src/main/java/com/epitech/pictmanager/PictmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.epitech.pictmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epitech/pictmanager/PictmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epitech.pictmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
