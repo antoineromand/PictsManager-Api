c29eaca8cc7ad0b1e42b16e1eb9c04591c2e263c
Antoine Romand <antoine.romand@epitech.eu>
2023-04-02 16:14:52 +0200

    Link Image and User

--- src/main/java/com/epitech/pictmanager/models/Image.java
+++ src/main/java/com/epitech/pictmanager/models/Image.java
@@ -18,11 +18,47 @@ public class Image {
     @Column(nullable = false)
     private String date;
 
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
