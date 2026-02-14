a345f481bbd22bedf243e96481ab41c46e11c01a
Antoine Romand <antoine.romand@epitech.eu>
2026-02-14 10:48:46 +0100

    feat: fix tests and add other. Remove useless exception.

--- src/main/java/com/epitech/pictmanager/modules/media_management/infrastructure/exceptions/MediaStorageException.java
+++
@@ -1,7 +0,0 @@
-package com.epitech.pictmanager.modules.media_management.infrastructure.exceptions;
-
-public class MediaStorageException extends RuntimeException {
-    public MediaStorageException(Throwable cause) {
-        super("Error while uploading file in storage.");
-    }
-}
