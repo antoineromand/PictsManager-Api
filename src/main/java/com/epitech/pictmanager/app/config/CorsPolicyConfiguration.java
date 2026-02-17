feb87f0e12d8ca95b0a98f8945ef91f0611c16f6
Antoine Romand <antoine.romand@epitech.eu>
2026-02-17 15:12:56 +0100

    feat: refactor dockerfile (use multi-step), update cors policy (use env variable and transform it to list, add s3 important info in .env, update docker-compose.yml

--- src/main/java/com/epitech/pictmanager/app/config/CorsPolicyConfiguration.java
+++ src/main/java/com/epitech/pictmanager/app/config/CorsPolicyConfiguration.java
@@ -1,19 +1,29 @@
 package com.epitech.pictmanager.app.config;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class CorsPolicyConfiguration {
 
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
+    private List<String> getListAllowedOrigins() {
+        return Arrays.stream(allowedOrigins.split(",")).map(String::trim).toList();
+    }
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(this.getListAllowedOrigins());
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
